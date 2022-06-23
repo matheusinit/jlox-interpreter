cba8b38106ee6fc99958b8ed3e0181bdd875de56
matheusinit <matheus.oliveira.s@protonmail.com>
2022-06-23 09:58:25 -0300

    implement parser

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -10,7 +10,9 @@ import java.nio.charset.Charset;
 import java.util.List;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
     
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -27,6 +29,7 @@ public class Lox {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
     
     private static void runPrompt() throws IOException {
@@ -45,10 +48,12 @@ public class Lox {
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
 
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+        if (hadError) return;
+
+        interpreter.interpret(expression);
     } 
     
     static void error(int line, String message) {
@@ -56,7 +61,20 @@ public class Lox {
     }
     
     private static void report(int line, String where, String message) {
-        System.err.println("[line " + "] Error" + where + ": " + message);
+        System.err.println("[line "+ line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at " + token.lexeme + "'", message);
+        }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + " ]");
+        hadRuntimeError = true;
     }
 }
